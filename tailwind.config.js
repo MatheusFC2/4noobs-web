abbd6e393ddc643113867171813755fbe45b8efb
MatheusFC2 <matheusfreitascampos@gmail.com>
2022-07-02 15:43:07 -0300

    chore: added darkmode class and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
@@ -10,8 +11,10 @@ module.exports = {
         'header': 'Staatliches, cursive'
       },
       colors: {
+        'white-f5': '#F5F5F5',
+        'dark-purple': '#1E0B35',
+        'dark-0d': '#0D0D0D',
         'purple-4noobs': '#6814CA',
-        'dark-purple': '#1E0B35'
       }
     },
   },
