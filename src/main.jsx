7b75bd713b8204a869551f98993ab6d145404a74
MatheusFC2 <matheusfreitascampos@gmail.com>
2022-07-03 21:22:37 -0300

    feat: created import theme hooks and apply on main

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import ThemeContextProvider from './hooks/useTheme'
+import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeContextProvider>
+      <App />
+    </ThemeContextProvider>
   </React.StrictMode>
 )
