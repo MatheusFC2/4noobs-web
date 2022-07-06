eefc6e98c493897c75866f56759b8940f5ff078f
MatheusFC2 <matheusfreitascampos@gmail.com>
2022-07-05 22:11:39 -0300

    style: add new var colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,12 @@ module.exports = {
       },
       colors: {
         'white-f5': '#F5F5F5',
+        'white-f3': '#F3F3F3',
+        'purple-4noobs': '#6814CA',
+        'purple-button': '#782BF1',
         'dark-purple': '#1E0B35',
         'dark-0d': '#0D0D0D',
-        'purple-4noobs': '#6814CA',
+        'dark-cards': '#191919',
       }
     },
   },
