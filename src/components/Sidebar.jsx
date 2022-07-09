3451b6b45bd133f2730bb5ced5dcafee8945f7fa
MatheusFC2 <matheusfreitascampos@gmail.com>
2022-07-09 07:00:11 -0300

    fix: fix text

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -19,45 +19,45 @@ function Sidebar () {
                     />
                 <div className="flex gap-x-4 items-center">
                     <img src={Logo} className={`cursor-pointer duration-500`} />
-                    <h1 className={`font-header text-white origin-left font-medium text-xl duration-300 ${!open && 'scale-0'}`}>
-                        H4ert Developer
+                    <h1 className={`font-header text-white origin-left font-medium text-xl  ${!open && 'scale-0 duration-700'}`}>
+                        He4rt Developer
                     </h1>
                 </div>
                 <div className="h-[1px] w-25 bg-[#2E0D55] mt-7"></div>
                 <ul className="pt-6">
                     <a href="/">
                         <div className="flex flex-row items-center pt-4">
-                            <House color="white" weight="bold" size={29}/><li className={`text-white text-lg pl-3 ${!open && 'scale-0'}`}>Home</li>
+                            <House color="white" weight="bold" size={29}/><li className={`text-white text-lg pl-3 ${!open && 'scale-0 duration-200'}`}>Home</li>
                         </div>
                     </a>
                     <a href="/Linguagens-de-Programacao" className="">
                         <div className="flex flex-row items-center pt-10">
-                            <Code color="white" weight="bold" size={29}/><li className={`text-white text-lg pl-3 ${!open && 'scale-0'}`}>Linguagens de Programação</li>
+                            <Code color="white" weight="bold" size={29}/><li className={`text-white text-lg pl-3 ${!open && 'scale-0 duration-200'}`}>Linguagens de Programação</li>
                         </div>
                     </a>
                     <a href="/Framework">
                         <div className="flex flex-row items-center pt-10">
-                            <Suitcase color="white" weight="bold" size={29}/><li className={`text-white text-lg pl-3 ${!open && 'scale-0'}`}>Framework</li>
+                            <Suitcase color="white" weight="bold" size={29}/><li className={`text-white text-lg pl-3 ${!open && 'scale-0 duration-200'}`}>Framework</li>
                         </div>
                     </a>
                     <a href="/Ferramentas">
                         <div className="flex flex-row items-center pt-10">
-                            <Wrench color="white" weight="bold" size={29}/><li className={`text-white text-lg pl-3 ${!open && 'scale-0'}`}>Ferramentas</li>
+                            <Wrench color="white" weight="bold" size={29}/><li className={`text-white text-lg pl-3 ${!open && 'scale-0 duration-200'}`}>Ferramentas</li>
                         </div>
                     </a>
                     <a href="/Sistemas-Operacionais">
                         <div className="flex flex-row items-center pt-10">
-                            <LinuxLogo color="white" weight="bold" size={29}/><li className={`text-white text-lg pl-3 ${!open && 'scale-0'}`}>Sistema Operacionais</li>
+                            <LinuxLogo color="white" weight="bold" size={29}/><li className={`text-white text-lg pl-3 ${!open && 'scale-0 duration-200'}`}>Sistema Operacionais</li>
                         </div>
                     </a>
                     <a href="/Design">
                         <div className="flex flex-row items-center pt-10">
-                            <PaintBrushBroad color="white" weight="bold" size={29}/><li className={`text-white text-lg pl-3 ${!open && 'scale-0'}`}>Design</li>
+                            <PaintBrushBroad color="white" weight="bold" size={29}/><li className={`text-white text-lg pl-3 ${!open && 'scale-0 duration-200'}`}>Design</li>
                         </div>
                     </a>
                 </ul>
                 <div className="flex flex-row items-center justify-center pt-14">
-                    <a href="https://www.instagram.com/heartdevs/" target="_blank"><InstagramLogo color="white" weight="regular" size={20} className={`mr-3 ${!open && 'scale-0 duration-100'}`} /></a>
+                    <a href="https://www.instagram.com/heartdevs/" target="_blank"><InstagramLogo color="white" weight="regular" size={20} className={`mr-3 ${!open && 'scale-0 duration-700'}`} /></a>
                     <a href="https://discord.gg/he4rt" target="_blank"><DiscordLogo color="white" weight="regular" size={20} className={`mr-3 ${!open && 'scale-0 duration-100'}`}/></a>
                     <a href="https://twitter.com/He4rtDevs" target="_blank"><TwitterLogo color="white" weight="regular" size={20} className={`mr-3 ${!open && 'scale-0 duration-100'}`} /></a>
                     <a href="https://heartdevs.com/" target="_blank"><Globe color="white" weight="regular" size={20} className={`${!open && 'scale-0 duration-100'}`} /></a>
@@ -67,7 +67,7 @@ function Sidebar () {
                     
                 </div>
                 <div className="flex justify-center items-center pt-10">
-                    {!open === false ? <h1 className="text-white font-header ">Desenvolvido por <a className="underline hover:text-purple-500 " href="https://omatheusfc.vercel.app/" target="_blank">omatheusfc</a></h1> 
+                    {!open === false ? <h1 className="text-white font-header">Desenvolvido por <a className="underline hover:text-purple-500 " href="https://omatheusfc.vercel.app/" target="_blank">omatheusfc</a></h1> 
                     : <h1 className="text-white font-header hover:text-purple-500"><a className="underline" href="https://omatheusfc.vercel.app/" target="_blank">OMFC</a></h1>}
                 
                 </div>
