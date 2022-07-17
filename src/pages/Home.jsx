d035d54610aba60b0ba526c26a095aae0b958ea8
MatheusFC2 <matheusfreitascampos@gmail.com>
2022-07-17 12:28:18 -0300

    chore: set img for cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import LogoLight from '../public/assets/4noobs-logo-light.svg'
 import LogoDark from '../public/assets/4noobs-logo-dark.svg'
 import Jslogo from '../public/assets/jslogo.png';
+import Clogo from '../public/assets/clogo.png';
+import Csharplogo from '../public/assets/csharplogo.jpg';
+import Bashlogo from '../public/assets/bashlogo.jpg';
 import { useTheme } from '../hooks/useTheme';
 import Sidebar from '../components/Sidebar';
 import { useEffect, useState } from 'react';
@@ -60,11 +63,74 @@ function Home() {
 
                     <div class="flex flex-wrap justify-evenly gap-5">
 
-                        {courses.slice(0, 4).map((course) => {
+                        {courses.slice(0, 1).map((course) => {
                             return (
                                 <div class="max-w-xs bg-white rounded-lg drop-shadow-[0_5px_3px_rgba(0,0,0,0.30)] dark:bg-dark-cards">
                                     <a href="#">
-                                        <img class="rounded-t-lg" src={Jslogo} alt="" />
+                                        <img class="rounded-t-lg" src="/src/public/assets/jslogo.png" alt="" />
+                                    </a>
+                                    <div class="p-5">
+                                        <a href="#">
+                                            <h5 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white font-header">{course.name}</h5>
+                                        </a>
+                                        <p className="text-sm pb-2 dark:text-white">{course.category}</p>
+                                        <div className="h-[2px] w-60 bg-slate-300 rounded-lg justify-center items-center dark:bg-zinc-800"></div>
+                                        <p class="pt-2 mb-3 text-gray-700 dark:text-gray-200">{course.description}</p>
+                                        <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-purple-button dark:hover:text-black border-2 border-purple-button rounded-lg hover:bg-purple-button hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:g-purple-button dark:hover:bg-puple-button dark:focus:bg-purple-button">
+                                            Ler Mais
+                                            <svg class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                        </a>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                        {courses.slice(1, 2).map((course) => {
+                            return (
+                                <div class="max-w-xs bg-white rounded-lg drop-shadow-[0_5px_3px_rgba(0,0,0,0.30)] dark:bg-dark-cards">
+                                    <a href="#">
+                                        <img class="rounded-t-lg" src={Bashlogo} alt="" />
+                                    </a>
+                                    <div class="p-5">
+                                        <a href="#">
+                                            <h5 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white font-header">{course.name}</h5>
+                                        </a>
+                                        <p className="text-sm pb-2 dark:text-white">{course.category}</p>
+                                        <div className="h-[2px] w-60 bg-slate-300 rounded-lg justify-center items-center dark:bg-zinc-800"></div>
+                                        <p class="pt-2 mb-3 text-gray-700 dark:text-gray-200">{course.description}</p>
+                                        <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-purple-button dark:hover:text-black border-2 border-purple-button rounded-lg hover:bg-purple-button hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:g-purple-button dark:hover:bg-puple-button dark:focus:bg-purple-button">
+                                            Ler Mais
+                                            <svg class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                        </a>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                        {courses.slice(2, 3).map((course) => {
+                            return (
+                                <div class="max-w-xs bg-white rounded-lg drop-shadow-[0_5px_3px_rgba(0,0,0,0.30)] dark:bg-dark-cards">
+                                    <a href="#">
+                                        <img class="rounded-t-lg" src={Clogo} alt="" />
+                                    </a>
+                                    <div class="p-5">
+                                        <a href="#">
+                                            <h5 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white font-header">{course.name}</h5>
+                                        </a>
+                                        <p className="text-sm pb-2 dark:text-white">{course.category}</p>
+                                        <div className="h-[2px] w-60 bg-slate-300 rounded-lg justify-center items-center dark:bg-zinc-800"></div>
+                                        <p class="pt-2 mb-3 text-gray-700 dark:text-gray-200">{course.description}</p>
+                                        <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-purple-button dark:hover:text-black border-2 border-purple-button rounded-lg hover:bg-purple-button hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:g-purple-button dark:hover:bg-puple-button dark:focus:bg-purple-button">
+                                            Ler Mais
+                                            <svg class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                        </a>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                        {courses.slice(3, 4).map((course) => {
+                            return (
+                                <div class="max-w-xs bg-white rounded-lg drop-shadow-[0_5px_3px_rgba(0,0,0,0.30)] dark:bg-dark-cards">
+                                    <a href="#">
+                                        <img class="rounded-t-lg" src={Csharplogo} alt="" />
                                     </a>
                                     <div class="p-5">
                                         <a href="#">
