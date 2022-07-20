f7348b5331145793a48a848dc4417d83bb702a3b
MatheusFC2 <matheusfreitascampos@gmail.com>
2022-07-20 13:51:54 -0300

    chore: building new UI and UX

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,20 +1,13 @@
-import { Route, Routes } from "react-router-dom";
-import { Design } from "../pages/Design";
-import { Ferramentas } from "../pages/Ferramentas";
-import { Framework } from "../pages/Framework";
-import Home from "../pages/Home";
-import { Ldp } from "../pages/Ldp";
-import { So } from "../pages/So";
+import { Route, Routes } from 'react-router-dom';
+import React from 'react';
+import Home from '../pages/Home';
 
-export function Router() {
-    return (
-        <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/linguagens-de-programacao" element={<Ldp/>} />
-            <Route path="/frameworks" element={<Framework/>} />
-            <Route path="/ferramentas" element={<Ferramentas/>} />
-            <Route path="/sistemas-operacionais" element={<So/>} />
-            <Route path="/design" element={<Design/>} />
-        </Routes>
-    )
+function Router() {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
+  );
 }
+
+export default Router;
