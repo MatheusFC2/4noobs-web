ddd06fb997c18b21912ed84aebe7d528c4679b9c
MatheusFC2 <matheusfreitascampos@gmail.com>
2022-07-20 17:46:26 -0300

    feat: modiified colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,8 +13,8 @@ module.exports = {
       colors: {
         'white-f5': '#F5F5F5',
         'white-f3': '#F3F3F3',
-        'purple-4noobs': '#6814CA',
-        'purple-button': '#782BF1',
+        'purple-hover': '#782BF1',
+        'purple-bg': '#6814CA',
         'dark-purple': '#1E0B35',
         'dark-0d': '#0D0D0D',
         'dark-cards': '#191919',
